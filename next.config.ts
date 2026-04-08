e9ae0bc0305f1145782026c90470589836c0cded
roman krasilnikov <krasilnikovroman5@gmail.com>
2026-04-08 21:57:03 +0700

    config update, unused images deleted

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images:{
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn2.thecatapi.com',
+        port: '',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname:' *.media.tumblr.com'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
